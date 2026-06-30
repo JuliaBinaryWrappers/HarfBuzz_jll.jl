9e2981f072fa6cc0a0ddb395604d8c5b5c15609a
jlbuild <juliabuildbot@gmail.com>
2026-06-30 17:45:06 +0000

    HarfBuzz_jll build 100.14002.1+0

--- src/HarfBuzz_jll.jl
+++ src/HarfBuzz_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("HarfBuzz")
-JLLWrappers.@generate_main_file("HarfBuzz", UUID("2e76f6c2-a576-52d4-95c1-20adfe4de566"))
+JLLWrappers.@generate_main_file("HarfBuzz", Base.UUID("2e76f6c2-a576-52d4-95c1-20adfe4de566"))
 end  # module HarfBuzz_jll
